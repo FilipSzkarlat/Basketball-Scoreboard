488207d56a56544e65160e2f260b7df3e09d84b6
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-09-26 10:50:48 +0200

    added a NEW GAME button and highlight for the leader

--- app.js
+++ app.js
@@ -6,35 +6,63 @@ const ThreeForHomeEl = document.querySelector(".threeForHome");
 const ThreeForGuestEl = document.querySelector(".threeForGuest");
 const scoreHomeEL = document.querySelector(".scoreHome");
 const scoreGuestEL = document.querySelector(".scoreGuest");
+const newBtn = document.querySelector(".new-btn");
 let scoreHome = 0;
 let scoreGuest = 0;
 
 OneForHomeEl.addEventListener("click", function () {
   scoreHome++;
   scoreHomeEL.textContent = scoreHome;
+  checkTheLeader();
 });
 
 OneForGuestEl.addEventListener("click", function () {
   scoreGuest++;
   scoreGuestEL.textContent = scoreGuest;
+  checkTheLeader();
 });
 
 TwoForHomeEl.addEventListener("click", function () {
   scoreHome += 2;
   scoreHomeEL.textContent = scoreHome;
+  checkTheLeader();
 });
 
 TwoForGuestEl.addEventListener("click", function () {
   scoreGuest += 2;
   scoreGuestEL.textContent = scoreGuest;
+  checkTheLeader();
 });
 
 ThreeForHomeEl.addEventListener("click", function () {
   scoreHome += 3;
   scoreHomeEL.textContent = scoreHome;
+  checkTheLeader();
 });
 
 ThreeForGuestEl.addEventListener("click", function () {
   scoreGuest += 3;
   scoreGuestEL.textContent = scoreGuest;
+  checkTheLeader();
 });
+
+newBtn.addEventListener("click", function () {
+  scoreGuest = 0;
+  scoreHome = 0;
+  scoreGuestEL.textContent = scoreGuest;
+  scoreHomeEL.textContent = scoreHome;
+  checkTheLeader();
+});
+
+function checkTheLeader() {
+  if (scoreGuest > scoreHome) {
+    scoreGuestEL.className = "scoreGuest highlight";
+    scoreHomeEL.classList.remove("highlight");
+  } else if (scoreHome > scoreGuest) {
+    scoreHomeEL.className = "scoreHome highlight";
+    scoreGuestEL.classList.remove("highlight");
+  } else {
+    scoreGuestEL.classList.remove("highlight");
+    scoreHomeEL.classList.remove("highlight");
+  }
+}
